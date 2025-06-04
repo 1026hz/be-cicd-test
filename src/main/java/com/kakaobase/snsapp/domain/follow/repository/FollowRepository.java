b0cd403cac954966a79c4f7cc81af6c1b0982cfa
hyup98 <hyup98@naver.com>
2025-06-04 18:16:16 +0900

    [FEAT] 팔로워, 팔로잉 Entity기반 findby메서드 구현

--- src/main/java/com/kakaobase/snsapp/domain/follow/repository/FollowRepository.java
+++ src/main/java/com/kakaobase/snsapp/domain/follow/repository/FollowRepository.java
@@ -6,8 +6,12 @@ import com.kakaobase.snsapp.domain.members.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
     boolean existsByFollowerUserAndFollowingUser(Member followerUser, Member followingUser);
+
+    Optional<Follow> findByFollowerUserAndFollowingUser(Member followerUser, Member followingUser);
 }
