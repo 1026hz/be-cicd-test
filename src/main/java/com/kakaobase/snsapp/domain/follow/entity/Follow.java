a0befeb348783905a58d4e5ada477a787cfa60a3
hyup98 <hyup98@naver.com>
2025-06-04 15:59:57 +0900

    [FEAT] Follow Entity구현

---
+++ src/main/java/com/kakaobase/snsapp/domain/follow/entity/Follow.java
@@ -0,0 +1,36 @@
+package com.kakaobase.snsapp.domain.follow.entity;
+
+
+import com.kakaobase.snsapp.domain.members.entity.Member;
+import com.kakaobase.snsapp.global.common.entity.BaseCreatedTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Follow extends BaseCreatedTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "follower_user_id", nullable = false)
+    private Member followerUser;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "following_id", nullable = false)
+    private Member followingUser;
+
+    @Builder
+    public Follow(Long id, Member followerUser, Member followingUser) {
+        this.id = id;
+        this.followerUser = followerUser;
+        this.followingUser = followingUser;
+    }
+
+}
