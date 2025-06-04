c9177ab97f698266c26aacf80e8db6cedcfe8a72
hyup98 <hyup98@naver.com>
2025-06-04 16:01:26 +0900

    [FEAT] Follow Controller구현

---
+++ src/main/java/com/kakaobase/snsapp/domain/follow/controller/FollowController.java
@@ -0,0 +1,69 @@
+package com.kakaobase.snsapp.domain.follow.controller;
+
+
+import com.kakaobase.snsapp.domain.auth.principal.CustomUserDetails;
+import com.kakaobase.snsapp.domain.comments.dto.CommentResponseDto;
+import com.kakaobase.snsapp.domain.follow.service.FollowService;
+import com.kakaobase.snsapp.global.common.response.CustomResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+@Tag(name = "팔로우 API", description = "팔로잉, 팔로우 관련 API")
+public class FollowController {
+
+    private final FollowService followService;
+
+    @PostMapping("{targetUserId}/follows")
+    @Operation(
+            summary = "팔로우 요청",
+            description = "유저에게 팔로우를 요청합니다"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "팔로우 요청 성공",
+                    content = @Content(schema = @Schema(implementation = CommentResponseDto.CreateCommentResponse.class))),
+            @ApiResponse(responseCode = "401", description = "로그인이 필요한 요청"),
+            @ApiResponse(responseCode = "404", description = "해당 유저를 찾을 수 없음"),
+            @ApiResponse(responseCode = "409", description = "이미 팔로우 한 유저")
+    })
+    public CustomResponse<?> createFollowing(
+            @PathVariable Long targetUserId,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+
+        followService.addFollowing(targetUserId, userDetails);
+
+        return CustomResponse.success("팔로우가 성공적으로 완료되었습니다.");
+    }
+
+    @DeleteMapping("{targetUserId}/follows")
+    @Operation(
+            summary = "팔로우 요청",
+            description = "유저에게 팔로우를 요청합니다"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "팔로우 취소 성공",
+                    content = @Content(schema = @Schema(implementation = CommentResponseDto.CreateCommentResponse.class))),
+            @ApiResponse(responseCode = "401", description = "로그인이 필요한 요청"),
+            @ApiResponse(responseCode = "404", description = "해당 유저를 찾을 수 없음"),
+            @ApiResponse(responseCode = "409", description = "이미 언팔로우 한 유저")
+    })
+    public CustomResponse<?> deleteFollowing(
+            @PathVariable Long targetUserId,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+
+        return CustomResponse.success("팔로우를 성공적으로 취소하였습니다.");
+    }
+}
