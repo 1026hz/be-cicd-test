45826dfaa4276a6e68ab32de740b6150a9a26bc5
hyup98 <hyup98@naver.com>
2025-06-04 21:16:57 +0900

    [FEAT] 팔로잉 유저 목록 조회 메서드 추가

--- src/main/java/com/kakaobase/snsapp/domain/follow/repository/FollowRepository.java
+++ src/main/java/com/kakaobase/snsapp/domain/follow/repository/FollowRepository.java
@@ -4,8 +4,11 @@ package com.kakaobase.snsapp.domain.follow.repository;
 import com.kakaobase.snsapp.domain.follow.entity.Follow;
 import com.kakaobase.snsapp.domain.members.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +17,21 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
     boolean existsByFollowerUserAndFollowingUser(Member followerUser, Member followingUser);
 
     Optional<Follow> findByFollowerUserAndFollowingUser(Member followerUser, Member followingUser);
+
+    @Query(value = """
+    SELECT m.id, m.nickname, m.name, m.profile_image
+    FROM follow f
+    JOIN member m ON f.follower_user_id = m.id
+    WHERE f.following_id = :followingId
+      AND (:cursor IS NULL OR m.id > :cursor)
+    ORDER BY m.id ASC
+    LIMIT :limit
+    """, nativeQuery = true)
+    List<Object[]> findFollowersByFollowingUserWithCursor(
+            @Param("followingId") Long followingId,
+            @Param("limit") Integer limit,
+            @Param("cursor") Long cursor
+
+    );
+
 }
